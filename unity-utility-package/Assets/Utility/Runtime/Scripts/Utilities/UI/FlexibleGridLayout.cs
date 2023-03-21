9bfd02a0a482f6eb9afd30a2c96e8f9c095c9e36
Wouter Spaas <wouter.spaas@mrwatts.io>
2023-03-21 16:42:10 +0100

    FEAT: #3 Added flexible grid layout and custom editor

---
+++ unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/UI/FlexibleGridLayout.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MrWatts.Internal.Utilities
+{
+    public sealed class FlexibleGridLayout : GridLayoutGroup
+    {
+        [SerializeField]
+        private int _columnCount = 12;
+        public int ColumnCount
+        {
+            get => _columnCount;
+            set
+            {
+                _columnCount = value;
+                UpdateCellSize();
+            }
+        }
+
+        [SerializeField]
+        private int _rowCount = 20;
+        public int RowCount
+        {
+            get => _rowCount;
+            set
+            {
+                _rowCount = value;
+                UpdateCellSize();
+            }
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            UpdateCellSize();
+            base.SetLayoutHorizontal();
+        }
+
+        public override void SetLayoutVertical()
+        {
+            UpdateCellSize();
+            base.SetLayoutVertical();
+        }
+
+        private void UpdateCellSize()
+        {
+            float x = (rectTransform.rect.size.x - padding.horizontal - (spacing.x * (ColumnCount - 1))) / ColumnCount;
+            float y = (rectTransform.rect.size.y - padding.vertical - (spacing.y * (RowCount - 1))) / RowCount;
+
+            if (startAxis == Axis.Horizontal)
+            {
+                constraint = Constraint.FixedColumnCount;
+                constraintCount = ColumnCount;
+            }
+            else
+            {
+                constraint = Constraint.FixedRowCount;
+                constraintCount = RowCount;
+            }
+
+            cellSize = new Vector2(x, y);
+        }
+    }
+}
---
+++ unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/UI/FlexibleGridLayoutCustomEditor.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace MrWatts.Internal.Utilities
+{
+    [CustomEditor(typeof(FlexibleGridLayout), true)]
+    [CanEditMultipleObjects]
+    public sealed class FlexibleGridLayoutCustomEditor : Editor
+    {
+        internal SerializedProperty m_Padding;
+        internal SerializedProperty m_Spacing;
+        internal SerializedProperty m_StartCorner;
+        internal SerializedProperty m_StartAxis;
+        internal SerializedProperty m_ChildAlignment;
+        internal SerializedProperty _columnCount;
+        internal SerializedProperty _rowCount;
+
+        private void OnEnable()
+        {
+            m_Padding = serializedObject.FindProperty("m_Padding");
+            m_Spacing = serializedObject.FindProperty("m_Spacing");
+            m_StartCorner = serializedObject.FindProperty("m_StartCorner");
+            m_StartAxis = serializedObject.FindProperty("m_StartAxis");
+            m_ChildAlignment = serializedObject.FindProperty("m_ChildAlignment");
+            _columnCount = serializedObject.FindProperty("_columnCount");
+            _rowCount = serializedObject.FindProperty("_rowCount");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Padding, true);
+            EditorGUILayout.PropertyField(m_Spacing, true);
+            EditorGUILayout.PropertyField(m_StartCorner, true);
+            EditorGUILayout.PropertyField(m_StartAxis, true);
+            EditorGUILayout.PropertyField(m_ChildAlignment, true);
+
+            EditorGUILayout.PropertyField(_columnCount, true);
+            EditorGUILayout.PropertyField(_rowCount, true);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
