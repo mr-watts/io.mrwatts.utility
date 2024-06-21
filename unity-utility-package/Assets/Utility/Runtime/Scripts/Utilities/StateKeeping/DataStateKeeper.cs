5c235cff41d95935e43ea54a11b3e8f890fb2c59
NoTuxNoBux <tux@mrwatts.io>
2024-06-21 17:07:20 +0200

    FEAT: Add data state keeper classes to maintain global app state
    These can then be injected through the service container.

---
+++ unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/StateKeeping/DataStateKeeper.cs
@@ -0,0 +1,7 @@
+namespace MrWatts.Internal.Utilities
+{
+    public sealed class DataStateKeeper<T> : IDataStateKeeper<T>
+    {
+        public T? State { get; set; }
+    }
+}
---
+++ unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/StateKeeping/EventEmittingDataStateKeeper.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace MrWatts.Internal.Utilities
+{
+    public sealed class EventEmittingDataStateKeeper<T> : IDataStateKeeper<T>
+    {
+        public T? State
+        {
+            get
+            {
+                return delegatee.State;
+            }
+            set
+            {
+                T? oldValue = delegatee.State;
+                delegatee.State = value;
+                OnValueChanged?.Invoke(this, new DataStateKeeperValueChangedEventArgs<T>(oldValue, value));
+            }
+        }
+
+        public event EventHandler<DataStateKeeperValueChangedEventArgs<T>>? OnValueChanged;
+
+        private readonly IDataStateKeeper<T> delegatee;
+
+        public EventEmittingDataStateKeeper(IDataStateKeeper<T> delegatee)
+        {
+            this.delegatee = delegatee;
+        }
+    }
+}
