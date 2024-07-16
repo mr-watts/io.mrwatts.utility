995233cea9689965a6f01b61ea704a47e43a1968
NoTuxNoBux <tux@mrwatts.io>
2024-07-16 14:50:48 +0200

    FIX: Allow initialization of DataStateKeeper with specific value

--- unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/StateKeeping/DataStateKeeper.cs
+++ unity-utility-package/Assets/Utility/Runtime/Scripts/Utilities/StateKeeping/DataStateKeeper.cs
@@ -3,5 +3,10 @@ namespace MrWatts.Internal.Utilities
     public sealed class DataStateKeeper<T> : IDataStateKeeper<T>
     {
         public T State { get; set; }
+
+        public DataStateKeeper(T initialValue = default)
+        {
+            State = initialValue;
+        }
     }
 }
